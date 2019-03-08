5aee312bedc37e7bdc75344c56b56eda8daa5e58
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2019-03-08 14:29:08 +0100

    Use register to store minimum distance

--- project.vhd
+++ project.vhd
@@ -21,13 +21,13 @@ architecture Behavioral of project_reti_logiche is
 -- the first one is the IDLE state, which waits for the start signal
 type state is (IDLE, LOAD_XP, LOAD_YP, STORE_XP, STORE_YP, LOAD_MASK, STORE_MASK, CHECK_MASK, LOAD_XC, LOAD_YC, STORE_XC, STORE_YC, COMPUTE_DIST, CHECK_DIST, WRITE_RES, SIM_END);
 
-signal P_X, P_Y, C_X, C_Y, MASK, O_MASK_CURR, O_MASK_NEXT: std_logic_vector(7 downto 0) := "00000000";
+signal P_X, P_Y, C_X, C_Y, MASK, omask_curr, omask_next: std_logic_vector(7 downto 0) := "00000000";
 
 signal centroid_curr, centroid_next: std_logic_vector(2 downto 0) := "000";
 
 signal state_curr, state_next : state := IDLE;
 
-signal min_dist, temp_dist: std_logic_vector (8 downto 0) := "000000000";  
+signal dmin_curr, dmin_next, dtemp: std_logic_vector (8 downto 0) := "000000000";  
 
 signal done_curr, done_next : std_logic := '0';
     
@@ -37,14 +37,16 @@ signal done_curr, done_next : std_logic := '0';
     process(i_clk, i_rst)
     begin
         if(i_rst = '1') then
-           centroid_curr <= "000";
-           O_MASK_CURR <= "00000000";
+           centroid_curr <= (others => '0');
+           omask_curr <= (others => '0');
            done_curr <= '0'; 
+           dmin_curr <= (others => '0');
            state_curr <= IDLE;
         elsif(rising_edge(i_clk)) then            
             centroid_curr <= centroid_next;
-            O_MASK_CURR <= O_MASK_NEXT;
+            omask_curr <= omask_next;
             done_curr <= done_next;
+            dmin_curr <= dmin_next;
             state_curr <= state_next;
         end if;
     end process;
@@ -69,16 +71,19 @@ signal done_curr, done_next : std_logic := '0';
     NEXTSTATE_PROCESS : process(state_curr, i_start, i_rst, centroid_curr)
     begin
         if(i_rst = '1') then
-            centroid_next <= "000";
+            centroid_next <= (others => '0');
             done_next <= '0';
-            O_MASK_NEXT <= "00000000";
-            min_dist <= "000000000";            
+            omask_next <= (others => '0');
+            dmin_next <= (others => '0');      
             state_next <= IDLE;
         else 
-            O_MASK_NEXT <= O_MASK_CURR;
-            min_dist <= min_dist;       
+
+            omask_next <= omask_curr;
+            dmin_next <= dmin_curr;       
             centroid_next <= centroid_curr;
+            dmin_next <= dmin_curr;
             done_next <= '0';
+
             case state_curr is       
                 when IDLE =>
                             if(i_start = '1') then
@@ -112,18 +117,18 @@ signal done_curr, done_next : std_logic := '0';
                 when STORE_XC => state_next <= LOAD_YC;
                 when STORE_YC => state_next <= COMPUTE_DIST;
                                                                     
-                when COMPUTE_DIST => temp_dist <= std_logic_vector(abs(signed(unsigned('0'&P_X) - unsigned('0'&C_X))) + abs(signed(unsigned('0'&P_Y) - unsigned('0'&C_Y))));
+                when COMPUTE_DIST => dtemp <= std_logic_vector(abs(signed(unsigned('0'&P_X) - unsigned('0'&C_X))) + abs(signed(unsigned('0'&P_Y) - unsigned('0'&C_Y))));
                                      state_next <= CHECK_DIST;
                          
                          -- The distance is not initialized -> store & save
-                when CHECK_DIST =>  if(min_dist = "000000000" or temp_dist < min_dist) then 
-                                        O_MASK_NEXT <= "00000000"; -- clear the output mask;                                    
-                                        O_MASK_NEXT(to_integer(unsigned(centroid_curr))) <= '1';
-                                        min_dist <= temp_dist;
-                                    elsif(min_dist = temp_dist) then
-                                        O_MASK_NEXT(to_integer(unsigned(centroid_curr))) <= '1';
+                when CHECK_DIST =>  if(dmin_curr = "000000000" or dtemp < dmin_curr) then 
+                                        omask_next <= (others => '0'); -- clear the output mask;                                    
+                                        omask_next(to_integer(unsigned(centroid_curr))) <= '1';
+                                        dmin_next <= dtemp;
+                                    elsif(dmin_curr = dtemp) then
+                                        omask_next(to_integer(unsigned(centroid_curr))) <= '1';
                                     else 
-                                        O_MASK_NEXT(to_integer(unsigned(centroid_curr))) <= '0';
+                                        omask_next(to_integer(unsigned(centroid_curr))) <= '0';
                                     end if;
                                     centroid_next <= std_logic_vector(unsigned(centroid_curr) + 1);
                                     state_next <= CHECK_MASK;          
@@ -175,7 +180,7 @@ signal done_curr, done_next : std_logic := '0';
                     o_address <= "0000000000010011";
                     o_en <= '1';
                     o_we <= '1';
-                    o_data <= O_MASK_CURR;
+                    o_data <= omask_curr;
         when SIM_END => 
                     o_address <= "0000000000000000";
                     o_en <= '0';
