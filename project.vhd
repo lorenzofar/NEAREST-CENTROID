95678fcd376c32790603a7363d8159a15f520beb
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2019-04-16 22:41:24 +0200

    Add register for mask signal to avoid inferring latches

--- project.vhd
+++ project.vhd
@@ -1,4 +1,4 @@
--- ===== SUBCOMPONENTS DESCRIPTION ===== --
+-- ===== MANHATTAN SUBCOMPONENT===== --
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -21,7 +21,7 @@ begin
         );
 end;
 
--- Main module description
+-- ===== MAIN MODULE ===== --
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
@@ -44,7 +44,7 @@ architecture Behavioral of project_reti_logiche is
 type state is (IDLE, LOAD_XP, LOAD_YP, STORE_XP, STORE_YP, LOAD_MASK, STORE_MASK, CHECK_MASK, LOAD_XC, LOAD_YC, STORE_XC, STORE_YC, CHECK_DIST, WRITE_RES, SIM_END);
 
 -- These signals are used to store the coordinates of the centroid and the main point, as well as the reference mask and output mask
-signal px_curr, px_next, py_curr, py_next, cx_curr, cx_next, cy_curr, cy_next, MASK, omask_curr, omask_next: unsigned(7 downto 0) := (others => '0');
+signal px_curr, px_next, py_curr, py_next, cx_curr, cx_next, cy_curr, cy_next, mask_curr, mask_next, omask_curr, omask_next: unsigned(7 downto 0) := (others => '0');
 
 -- These signals are used to keep track of the current centroid, in order to stop computation and to access vectors by index
 signal centroid_curr, centroid_next: unsigned(2 downto 0) := (others => '0');
@@ -77,22 +77,29 @@ begin
     -- When instead we are on the rising edge of the i_rst signal we put the system back in the initial state, ready to start a new simulation
     CLOCK_PROCESS : process(i_clk, i_rst)
     begin
-        if(i_rst = '1') then
-            centroid_curr <= (others => '0');
-            omask_curr <= (others => '0');
-            done_curr <= '0'; 
-            dmin_curr <= (others => '1');
-            state_curr <= IDLE;
-        elsif(rising_edge(i_clk)) then            
-            centroid_curr <= centroid_next;
-            omask_curr <= omask_next;
-            done_curr <= done_next;
-            dmin_curr <= dmin_next;
-            state_curr <= state_next;
+        if(rising_edge(i_clk)) then
+            -- Here we always update the coordinates, since we do not care to reset them
             px_curr <= px_next;
             py_curr <= py_next;
             cx_curr <= cx_next;
             cy_curr <= cy_next;
+            if(i_rst = '1') then
+                -- Here we have to reset the machine state
+                -- We reset only critical signals
+                centroid_curr <= (others => '0');
+                omask_curr <= (others => '0');
+                mask_curr <= (others => '0');
+                done_curr <= '0'; 
+                dmin_curr <= (others => '1');
+                state_curr <= IDLE;
+            else             
+                centroid_curr <= centroid_next;
+                omask_curr <= omask_next;
+                mask_curr <= mask_next;
+                done_curr <= done_next;
+                dmin_curr <= dmin_next;
+                state_curr <= state_next;
+            end if;
         end if;
     end process;
 
@@ -107,7 +114,7 @@ begin
         py_next <= py_curr;
         cx_next <= cx_curr;
         cy_next <= cy_curr;
-        MASK <= MASK;
+        mask_next <= mask_curr;
         case(state_curr) is
             when STORE_XP => 
                 px_next <= unsigned(i_data);
@@ -118,7 +125,7 @@ begin
             when STORE_YC => 
                 cy_next <= unsigned(i_data);
             when STORE_MASK => 
-                MASK <= unsigned(i_data);
+                mask_next <= unsigned(i_data);
             when others => null;
         end case;
     end process;
@@ -130,10 +137,10 @@ begin
     begin
         if(i_rst = '1') then
             -- Here we set all the next values of signals to 0, in order to reset the system and start again
+            omask_next <= (others => '0');
+            dmin_next <= (others => '1');      
             centroid_next <= (others => '0');
             done_next <= '0';
-            omask_next <= (others => '0');
-            dmin_next <= (others => '0');      
             state_next <= IDLE;
         else 
             -- Here we assign the current value of signals to the next one, in order to keep information
@@ -145,13 +152,14 @@ begin
             
             -- Then we determine in which state the FSM is 
             case state_curr is       
-                when IDLE =>
+                when IDLE =>                
                     if(i_start = '1') then
                         state_next <= LOAD_XP; -- The system received the start signal, we start the simulation
                     else 
                         state_next <= IDLE; -- We continue to wait for the i_start signal to be 1
                     end if;
     
+                -- Load working point coordinates and mask
                 when LOAD_XP => 
                     state_next <= STORE_XP;                   
                 when STORE_XP => 
@@ -164,12 +172,13 @@ begin
                     state_next <= STORE_MASK;
                 when STORE_MASK => 
                     state_next <= CHECK_MASK;
-                             
+                            
+                -- Check whether the current centroid has to be considered according to mask
                 when CHECK_MASK =>
-                    if( centroid_curr = "111" and MASK(to_integer(centroid_curr)) = '0') then 
+                    if(centroid_curr = "111" and mask_curr(to_integer(centroid_curr)) = '0') then 
                         state_next <= WRITE_RES;
                     else 
-                        if(MASK(to_integer(centroid_curr)) = '1') then                                        
+                        if(mask_curr(to_integer(centroid_curr)) = '1') then                                        
                             centroid_next <= centroid_curr;
                             state_next <= LOAD_XC;
                         else                              
@@ -187,7 +196,7 @@ begin
                     state_next <= LOAD_YC;
                 when STORE_YC => 
                     state_next <= CHECK_DIST;
-                         
+                        
                 -- The distance is not initialized -> store & save
                 when CHECK_DIST =>  
                     if(dmin_curr = "111111111" or dtemp < dmin_curr) then 
@@ -202,26 +211,26 @@ begin
                     
                     -- Check whether I arrived at last step
                     if(centroid_curr = to_unsigned(7, centroid_curr'length)) then
-                        centroid_next <= centroid_curr;
                         state_next <= WRITE_RES;
                     else                                                                                
-                        centroid_next <= centroid_curr + 1;                                    
+                        centroid_next <= centroid_curr + 1; -- Increase the centroid counter                               
                         state_next <= CHECK_MASK;
                     end if;                                     
 
                 when WRITE_RES =>   
                     done_next <= '1';
                     state_next <= SIM_END;
-                
+
                 when SIM_END => 
                     if(i_start = '1') then
+                        done_next <= '1';
                         state_next <= SIM_END;
                     else 
                         done_next <= '0';
                         state_next <= IDLE;
                     end if;         
             end case; 
-        end if;            
+        end if;      
     end process;
     
     OUT_PROCESS : process(state_curr)
@@ -244,15 +253,15 @@ begin
                         o_en <= '1';
             when LOAD_YC => 
                         o_address <= std_logic_vector(unsigned("000000000000"&centroid_curr&"0") + 2);
-                        o_en <= '1';
+                        o_en <= '1';                        
             when WRITE_RES => 
                         o_address <= std_logic_vector(to_unsigned(19, o_address'length));
                         o_en <= '1';
                         o_we <= '1';
                         o_data <= std_logic_vector(omask_curr);
             when SIM_END => 
-                        o_address <= (others => '0');
-                        o_en <= '0';
+                            o_address <= (others => '0');
+                            o_en <= '0';
             when others => 
                         o_address <= (others => '0');
                         o_en <= '0';
